350052c85a93e945f19819208d8a585a32cd2496
Nhat Minh Vo <nhat@nhats-mbp.home>
2023-04-03 19:55:40 -0400

    finished building spaceX app using given rest api in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { MaterialModule } from './material.component';
+import { HttpClientModule } from '@angular/common/http'
+import { SpacexapiService } from './network/spacexapi.service';
+import { MissionfilterComponent } from './missionfilter/missionfilter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissiondetailsComponent,
+    MissionlistComponent,
+    MissionfilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SpacexapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
